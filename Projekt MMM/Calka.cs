b767bc2e53c9374fe5039701055bf7ab7998c0cb
jskrzypczak97 <30375072+jskrzypczak97@users.noreply.github.com>
2018-04-02 23:58:51 +0200

    Wstępnie gotowy projekt

---
+++ Projekt MMM/Calka.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_MMM
+{
+    class Calka
+    {
+        public double probka_poprzednia = 0;
+        public double probka_terazniejsza = 0;
+        public double wartosc = 0;
+
+        public static double krok = 0.001;
+    }
+}
